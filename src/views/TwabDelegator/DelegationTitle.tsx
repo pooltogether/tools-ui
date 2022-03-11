20976de2e147c9e000625ed31326c904d8a4410d
Charles Bergeron <chuck.bergeron@gmail.com>
2022-03-11 13:20:39 -0800

    new font/etc

--- src/views/TwabDelegator/DelegationTitle.tsx
+++ src/views/TwabDelegator/DelegationTitle.tsx
@@ -3,11 +3,11 @@ import classNames from 'classnames'
 
 export const DelegationTitle: React.FC<{ className?: string }> = (props) => {
   return (
-    <div className={classNames(props.className, 'flex flex-col')}>
+    <div className={classNames(props.className, 'flex flex-col font-averta')}>
       <h5 className='mb-2'>Lend out your chances of winning to other Ethereum accounts</h5>
       <p className='text-accent-1 text-xs'>
-        Ex. BAYC’s treasury delegates $500,000 of PTUSDC. 50 random winners receive $10,000 worth of
-        PoolTogether chance for 3 months
+        Ex. CloneX's treasury delegates $500,000 of PTUSDC. 50 random winners receive $10,000 worth
+        of PoolTogether chance for 3 months
       </p>
       <a className='transition underline text-pt-teal hover:opacity-70 flex items-center space-x-1'>
         <span>Learn more</span>
