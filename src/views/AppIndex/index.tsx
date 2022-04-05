5c9303c2c7430a82c3875b2cee2c6a4d1bd2b8f3
Dylan DesRosier <dylan@pooltogether.com>
2022-04-05 16:32:25 -0400

    Localize everything

--- src/views/AppIndex/index.tsx
+++ src/views/AppIndex/index.tsx
@@ -1,13 +1,15 @@
 import Layout from '@components/Layout'
 import { PagePadding } from '@components/Layout/PagePadding'
 import { PageTitle } from '@components/Layout/PageTitle'
+import { useTranslation } from 'react-i18next'
 import { AppIndexList } from './AppIndexList'
 
 export const AppIndex: React.FC = (props) => {
+  const { t } = useTranslation()
   return (
     <Layout>
       <PagePadding>
-        <PageTitle title={'App Index'} />
+        <PageTitle title={t('appIndex', 'App Index')} />
         <AppIndexList />
       </PagePadding>
     </Layout>
