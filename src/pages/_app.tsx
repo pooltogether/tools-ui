68e13d8f802bf9891926fbe3c050da1bbd397e22
Dylan DesRosier <dylan@pooltogether.com>
2022-04-06 16:05:13 -0400

    Wrap app in loading state. No SSR for core app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import type { AppProps } from 'next/app'
-import { AppContainer } from '../components/AppContainer'
+import dynamic from 'next/dynamic'
+import { LoadingScreen } from '@components/LoadingScreen'
 
 // Styles must be imported in _app
 import '@styles/index.css'
 import 'react-toastify/dist/ReactToastify.css'
 import 'react-spring-bottom-sheet/dist/style.css'
 import '@styles/bottomSheet.css'
+
+const AppContainer = dynamic(
+  () => import('../components/AppContainer').then((mod) => mod.AppContainer),
+  {
+    ssr: false,
+    loading: () => <LoadingScreen />
+  }
+)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
