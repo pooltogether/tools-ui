513b1f92dbe766ccce010bb307552703da35413f
Dylan DesRosier <dylan@pooltogether.com>
2022-04-08 17:02:25 -0400

    Add missing strings

--- src/views/TwabDelegator/DelegationTitle.tsx
+++ src/views/TwabDelegator/DelegationTitle.tsx
@@ -1,21 +1,20 @@
 import FeatherIcon from 'feather-icons-react'
 import classNames from 'classnames'
 import { DELEGATION_LEARN_MORE_URL } from './constants'
+import { useTranslation } from 'react-i18next'
 
 export const DelegationTitle: React.FC<{ className?: string }> = (props) => {
+  const { t } = useTranslation()
   return (
     <div className={classNames(props.className, 'flex flex-col font-averta')}>
-      <p className='text-accent-1 text-xxs'>
-        When you delegate, you're giving the chosen address additional chances to win without losing
-        custody of your underlying deposit.
-      </p>
+      <p className='text-accent-1 text-xxs'>{t('delegationTitleDescription')}</p>
       <a
         className='transition underline text-pt-teal hover:opacity-70 flex items-center space-x-1 text-xxs'
         href={DELEGATION_LEARN_MORE_URL}
         target='_blank'
         rel='noreferrer'
       >
-        <span>Learn more</span>
+        <span>{t('learnMore')}</span>
         <FeatherIcon icon='external-link' className='w-3 h-3' />
       </a>
     </div>
