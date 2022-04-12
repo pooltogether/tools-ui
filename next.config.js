df50c189bcfd635dbaef8549c7955bfb848d348a
Charles Bergeron <chuck.bergeron@gmail.com>
2022-04-12 16:05:48 -0700

    change page titles

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const chalk = require("chalk")
 const path = require('path');
 
 const nextConfig = {
