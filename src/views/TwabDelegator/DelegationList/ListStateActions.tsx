58933dba4520df0837042bfb031afce035745c80
Dylan DesRosier <dylan@pooltogether.com>
2022-04-21 17:12:23 -0400

    Fix css

--- src/views/TwabDelegator/DelegationList/ListStateActions.tsx
+++ src/views/TwabDelegator/DelegationList/ListStateActions.tsx
@@ -204,7 +204,7 @@ const FixedFooterNav: React.FC<{ children: React.ReactElement }> = ({ children }
   return (
     <>
       {/* Desktop */}
-      <div className='hidden xs:flex w-full items-center justify-end mb-20'>{children}</div>
+      <div className='hidden xs:flex w-full items-center justify-between mb-20'>{children}</div>
       {/* Mobile */}
       <div className='flex xs:hidden items-center fixed b-0 l-0 r-0 h-20 bg-pt-purple-bright justify-between space-x-2 px-2 z-10'>
         {children}
