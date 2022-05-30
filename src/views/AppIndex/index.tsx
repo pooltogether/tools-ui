80c549fd52085a985a0c450083153107995e3d2e
Dylan DesRosier <dylan@pooltogether.com>
2022-05-30 11:45:44 -0400

    Add community links

--- src/views/AppIndex/index.tsx
+++ src/views/AppIndex/index.tsx
@@ -3,14 +3,25 @@ import { PagePadding } from '@components/Layout/PagePadding'
 import { PageTitle } from '@components/Layout/PageTitle'
 import { useTranslation } from 'react-i18next'
 import { AppIndexList } from './AppIndexList'
+import { CommunityAppIndexList } from './CommunityAppIndexList'
 
 export const AppIndex: React.FC = (props) => {
   const { t } = useTranslation()
   return (
     <Layout>
-      <PagePadding>
+      <PagePadding className='space-y-4'>
         <PageTitle title={t('tools', 'Tools')} />
         <AppIndexList />
+        <div className='leading-tight'>
+          <p className='font-bold mb-2'>{t('communityToolsTitle', 'Community Tools')}</p>
+          <p className='text-xxs sm:text-sm opacity-75'>
+            {t(
+              'communityToolsDescription',
+              'Tools built by the PoolTogether community for the PoolTogether community.'
+            )}
+          </p>
+        </div>
+        <CommunityAppIndexList />
       </PagePadding>
     </Layout>
   )
