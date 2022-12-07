826006be4d70fe789ba25c48f3c4b5dac2badd3f
Ncookiez <ncukiert@gmail.com>
2022-12-07 12:11:48 -0300

    Moved Current Content to ConfigurationView

---
+++ src/tools/PrizeTierController/ConfigurationView/index.tsx
@@ -0,0 +1,19 @@
+import { usePrizeTierHistoryContracts } from '@prizeTierController/hooks/usePrizeTierHistoryContracts'
+import { PrizeTierHistoryItem } from '@prizeTierController/ConfigurationView/PrizeTierHistoryItem'
+import classNames from 'classnames'
+
+export const ConfigurationView = (props: { className?: string }) => {
+  const { className } = props
+  const prizeTierHistoryContracts = usePrizeTierHistoryContracts()
+
+  return (
+    <ul className={classNames('grid grid-cols-1 sm:grid-cols-2 gap-4', className)}>
+      {prizeTierHistoryContracts.map((contract) => (
+        <PrizeTierHistoryItem
+          prizeTierHistoryContract={contract}
+          key={'pth-item-' + `${contract.id}`}
+        />
+      ))}
+    </ul>
+  )
+}
