0ea8de25e516b5f6bab32b5e61cef8a545ef3213
Ncookiez <ncukiert@gmail.com>
2022-12-07 16:26:47 -0300

    Added Pool TVL Hook

--- src/tools/PrizeTierController/ProjectionView/PrizePoolItem.tsx
+++ src/tools/PrizeTierController/ProjectionView/PrizePoolItem.tsx
@@ -1,4 +1,5 @@
 import { PrizePool } from '@pooltogether/v4-client-js'
+import { usePrizePoolTvl } from '@prizeTierController/hooks/usePrizePoolTvl'
 import { PrizePoolEditHistory } from '@prizeTierController/interfaces'
 import { PrizeTierHistoryTitle } from '@prizeTierController/PrizeTierHistoryTitle'
 
@@ -8,7 +9,7 @@ export const PrizePoolItem = (props: {
 }) => {
   const { prizePool, editHistory } = props
 
-  if (editHistory) {
+  if (editHistory && editHistory.prizeTierHistoryContract.isV2) {
     return (
       <li className='p-4 bg-actually-black bg-opacity-10 rounded-xl'>
         <PrizeTierHistoryTitle
@@ -27,6 +28,15 @@ const PrizePoolProjections = (props: {
   editHistory: PrizePoolEditHistory
 }) => {
   const { prizePool, editHistory } = props
+  const { data: tvl, isFetched: isFetchedTvl } = usePrizePoolTvl(prizePool)
+
+  // TODO: display pool TVL
+  // TODO: display estimated prize counts with DPR
+  // TODO: display estimated prize values with DPR
+  // TODO: show prizes over time chart
+  // TODO: display daily, weekly, monthly, and yearly estimate of prize amounts
+  // TODO: show dropped prizes estimates
+  // TODO: include variance slider
 
   return <></>
 }
